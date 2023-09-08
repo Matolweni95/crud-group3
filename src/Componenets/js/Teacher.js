4ab97b1e894ead4fc9347d6208df679ed96379f4
MaDladla <cynthiap16dladla@gmail.com>
2023-09-08 12:33:38 +0200

    Added edit on mark

--- src/Componenets/js/Teacher.js
+++ src/Componenets/js/Teacher.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import '../css/Teacher.css'
-import React, { useState } from 'react';
+import React, { useState,useRef  } from 'react';
 
 function Teacher() {
   const initialState = {
@@ -9,29 +9,43 @@ function Teacher() {
 
   const [markData, setMarkData] = useState(initialState);
   const [marksList, setMarksList] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const formRef = useRef(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const markValue = event.target.mark.value;
+    let markValue = event.target.mark.value;
 
 
     setMarksList([...marksList, markValue]);
-
-    console.log("Mark added:", markValue);
+    formRef.current.reset();
+   
   };
 
+
+
+  const handleChange = (newValue, index) => {
+    const updatedValues = [...marksList];
+    updatedValues[index] = newValue;
+    setMarksList(updatedValues);
+  };
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+  };
   return (
     <div class="container">
     <div class="row">
         <div class="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
             <h2>MARK</h2>
-            <form onSubmit={handleSubmit} id="markForm">
+            <form onSubmit={handleSubmit} id="markForm" ref={formRef}>
                 <div class="mb-3">
                     <input
                         type="text"
                         class="form-control"
                         placeholder="Add Mark"
                         id="mark"
+                      
                     />
                 </div>
                 <div class="Buttons">
@@ -59,9 +73,21 @@ function Teacher() {
                 {marksList.map((mark, index) => (
                 <tr key={index}>
                   <td>Learner {index + 1}</td>
-                  <td>{mark}</td>
-                  <td></td>
-                  <td></td>
+                  <td>  <input
+            type="text"
+            value={marksList[index]}
+            onChange={(e) => handleChange(e.target.value, index)}
+            disabled={index !== editIndex} 
+            className='markInput'
+          /> 
+        
+           </td>
+                  <td>     {index === editIndex ? (
+            <button onClick={() => setEditIndex(null)} className='editBtn'>Save</button>
+          ) : (
+            <button onClick={() => handleEdit(index)}  className='editBtn'>Edit</button>
+          )}</td>
+                 
                 </tr>
               ))}
                 </tbody>
