6ce4888869362301bc5a6fd1adfde1f6a74a9d6e
Phathu Mandane <mandanephathu@gmail.com>
2023-09-08 11:28:14 +0200

    Sign in

--- src/Componenets/js/Signin.js
+++ src/Componenets/js/Signin.js
@@ -1,16 +1,48 @@
 import React, {useState} from 'react';
 import '../css/Signin.css';
 import { Link } from 'react-router-dom';
+import { supabase } from './Supabase.js';
 
 const Signin = ({setIsLoggedIn}) => {
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-}
+  const [error, setError] = useState('');
+  const [showError, setShowError] = useState(null);
 
-const handleLogin = () => {
+/*const handleLogin = () => {
     setIsLoggedIn(true); 
+};*/
+
+
+const handleLogin = async () => {
+  //const name = document.getElementById('username').value; // Get username from user input
+  //const password = document.getElementById('password').value; // Get password from user input
+
+  try {
+  
+      const { data, error } = await supabase
+      .from('User')
+      .select('UserID')
+
+    if (error) {
+      throw new Error('Authentication failed');
+    }
+
+    // Assuming authentication was successful
+    const jwtToken = data.access_token;
+
+    // Log the token or set it in your app's state for authentication
+    console.log('JWT Token:', jwtToken);
+
+    // Set isLoggedIn to true to indicate successful login
+    setIsLoggedIn(true);
+  } catch (error) {
+    console.error('Sign-in error:', error);
+    // Handle authentication errors, display error messages.
+    setError('Authentication failed. Please check your username and password.');
+    setShowError(true);
+  }
 };
+
     
   return (
     <div className='login'>
@@ -19,7 +51,7 @@ const handleLogin = () => {
           <h1>Login</h1>
         </div>
         <div className="card__body">
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleLogin}>
             <div className="form__group">
               <label htmlFor="username">Username</label>
               <input type="text" id="username" name="username" required="" />
