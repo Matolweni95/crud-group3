fb4a3eb670c82683bb4996ff779167f138adb70d
Masixole <masixolematolweni.1@gmail.com>
2023-09-11 15:56:18 +0200

    signin/signout fix + usecontext

--- src/Componenets/js/Signout.js
+++ src/Componenets/js/Signout.js
@@ -2,9 +2,14 @@ import React from 'react';
 import '../css/Signout.css'
 
 const Signout = () => {
+
+  const handleSignOut = () => {
+    localStorage.removeItem('isLoggedIn', 'true');
+    window.location.reload();
+  }
   return (
     <div className='signout'>
-        <button className='btn'>Signout</button>
+        <button onClick={handleSignOut} className='btn'>Signout</button>
     </div>
   )
 }
